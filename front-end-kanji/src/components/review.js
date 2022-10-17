258e6a4a943628f2b5a2912bd96d7ab88abc288d
LeoKnox <LeoKnox@users.noreply.github.com>
2022-10-16 22:42:13 -0700

    Add files via upload

--- front-end-kanji/src/components/review.js
+++ front-end-kanji/src/components/review.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 function Review() {
     const { grades } = useParams();
 
+    console.log("****"+grades);
+
     return (
         <h3>now review {grades}</h3>
     )
