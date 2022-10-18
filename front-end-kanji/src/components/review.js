aeb63c81e061a6b3a4f27894d8b838f76c692250
LeoKnox <LeoKnox@users.noreply.github.com>
2022-10-17 22:07:13 -0700

    Add files via upload

--- front-end-kanji/src/components/review.js
+++ front-end-kanji/src/components/review.js
@@ -8,7 +8,6 @@ const Review = () => {
     const [allKanji, setAllKanji] = useState([]);
 
     useEffect(() => {
-        console.log(grades.grades);
         const x = `http://127.0.0.1:8000/quiz/${grades.id}`;
         axios.get(x)
             .then(function (response) {
@@ -20,9 +19,10 @@ const Review = () => {
             .then(function check() {
                 const quizList = allKanji.sort(() => Math.random() - 0.5).slice(0,6);
                 setQuiz(quizList);
+                console.log(quiz);
                 //ans = quizList[Math.floor(Math.random()*quizList.length)];
             })
-        }, []);
+        }, [grades.id]);
 
     console.log("****"+grades.id);
 
