9a5de3d1395d3ac6d7b0009ab8dbcc8933980603
LeoKnox <LeoKnox@users.noreply.github.com>
2022-10-17 22:01:52 -0700

    Add files via upload

--- front-end-kanji/src/components/review.js
+++ front-end-kanji/src/components/review.js
@@ -1,12 +1,33 @@
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Review = () => {
     const grades = useParams();
+    const [quiz, setQuiz] = useState([]);
+    const [allKanji, setAllKanji] = useState([]);
 
-    console.log("****"+JSON.stringify(grades));
+    useEffect(() => {
+        console.log(grades.grades);
+        const x = `http://127.0.0.1:8000/quiz/${grades.id}`;
+        axios.get(x)
+            .then(function (response) {
+                setAllKanji(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+            .then(function check() {
+                const quizList = allKanji.sort(() => Math.random() - 0.5).slice(0,6);
+                setQuiz(quizList);
+                //ans = quizList[Math.floor(Math.random()*quizList.length)];
+            })
+        }, []);
+
+    console.log("****"+grades.id);
 
     return (
-        <h3>now review {grades.id}</h3>
+        <h3>now review</h3>
     )
 }
 
