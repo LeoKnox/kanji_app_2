b18940e27410cc74807476bd9eaa488be8c9f218
LeoKnox <LeoKnox@users.noreply.github.com>
2022-10-20 22:40:56 -0700

    Add files via upload

--- front-end-kanji/src/components/index.js
+++ front-end-kanji/src/components/index.js
@@ -43,7 +43,7 @@ const Index = () => {
                 ))}
             </ul>
         </div>
-        <Test peas={grades} />
+        {grades && <Test peas={grades} />}
         <Outlet />
         </>
     )
